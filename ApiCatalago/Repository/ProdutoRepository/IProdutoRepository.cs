0c9669766114c870011d49c0d83dd46199badb15
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-02-24 18:11:59 -0300

    return the project to confirm the previous

--- ApiCatalago/Repository/ProdutoRepository/IProdutoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ApiCatalago.Models;
-
-namespace ApiCatalago.Repository.RepositoryProduto.ProdutoRepository
-{
-    public interface IProdutoRepository : IRepository<Produto>
-    {
-        IEnumerable<Produto> GetProductByPrice();
-    }
-}
